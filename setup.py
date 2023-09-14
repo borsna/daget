7cb6a3c8ace30d65c192212bae9a1fc26295a86f
aliirz <dejavu87@gmail.com>
2023-09-14 14:17:22 +0500

    package daget for pip #12

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='daget',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/borsna/daget',
+    license='MIT',
+    author='Olof Olsson',
+    author_email='your-email@example.com',
+    python_requires='>=3.6',
+    description='Download dataset via DOI or landing page url',
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'daget=daget.daget:main',
+        ],
+    },
+)
